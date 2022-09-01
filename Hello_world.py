2d44bea176f8de6f92b75f3fecb6ae21327189d1
GipeR <vicommsk@gmail.com>
2022-09-01 19:46:20 +0400

    start
    start

---
+++ Hello_world.py
@@ -0,0 +1,63 @@
+# data type
+# print(123) #integer - int
+# print(7.98) #floating - float
+# print('hello') #string - str
+
+# Арифиметические функции
+# print(2 + 2)
+# print(2 - 2)
+# print(2 * 2)
+# print(2 / 2)
+# print(2 ** 3) #2*2*2 возведение в степень
+# print(2 // 2) #деление с округление в int
+# print(2985 % 2) #от остатка - сколько останеться не разделенных
+
+
+# print(3 * 7)
+# print(4 ** 3)
+# print(29 % 5)
+
+# x = 5
+# print(x)
+# x = "hello!"
+# print(x)
+# x = 7.9
+# type_of_variable = type(x)
+# print(type_of_variable)
+#
+# car_color_1 = "black"
+# print(car_color_1)
+
+# x = 5
+# y = 3
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x // y)
+# print(x ** y)
+# print(x % y)
+
+#credit rate programm
+# credit = 7000
+# credit_rate = 3.5
+# number_of_years = 9
+# final_sum = credit + credit / 100 * credit_rate * number_of_years
+#
+# print(final_sum)
+
+# model_auto = "seat"
+# color_auto = "black"
+# year_auto = 2010
+# doors_auto = 5
+# color_auto = "white"
+# print(model_auto, color_auto, year_auto, doors_auto)
+
+# man_1 = 18
+# man_2 = 20
+# man_3 = 22
+# man_4 = 24
+# man_5 = 26
+# man_all = 5
+# x = man_1 + man_2 + man_3 + man_4 + man_5
+# print(x / man_all)
